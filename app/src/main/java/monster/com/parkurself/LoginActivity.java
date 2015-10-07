76c94bf3d1158b7f940f94dfdbb2a5ba357d47d4
Monster-Zou <zouyu930503@ufl.edu>
2015-10-07 02:11:28 -0400

    Second Version

--- app/src/main/java/monster/com/parkurself/LoginActivity.java
+++ app/src/main/java/monster/com/parkurself/LoginActivity.java
@@ -2,15 +2,38 @@ package monster.com.parkurself;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button button_login;
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    public LoginInfo loginInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //check the login information
+        button_login=(Button)findViewById(R.id.button_login);
+        accountEdit=(EditText)findViewById(R.id.account);
+        passwordEdit=(EditText)findViewById(R.id.password);
+        loginInfo=new LoginInfo();
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginInfo.account=accountEdit.getText().toString();
+                loginInfo.password=passwordEdit.getText().toString();
+                Log.d("Parkurself","account is :"+loginInfo.account+"password is:"+loginInfo.password);
+                new CheckLoginInfo().execute(loginInfo);
+            }
+        });
     }
 
     @Override
@@ -34,4 +57,5 @@ public class LoginActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
